1ce8b6ef269e2f189a0d1151d7319be6cdbf8b68
Omar Emara <hn23952@bristol.ac.uk>
2023-10-14 21:28:40 +0100

    initial commit

---
+++ construct_pole_balancing_env.py
@@ -0,0 +1,13 @@
+"""
+This module contains the functions required to construct the environment for 
+the RL agent to interact with. The following methods are included:
+    - env_params_setup
+    - state_space_define
+    - state_transition_function
+    - reward_function
+    - episode_loop
+    - env_reset
+
+@author: hn23952
+"""
+
