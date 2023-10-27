0e46d58e60355b775021b91306866dcc7ea38f84
Jonathan Raines <jr0278@bristol.ac.uk>
2023-10-27 13:18:26 +0100

    Add test file that does not meet Ruff spec

---
+++ unlinted.py
@@ -0,0 +1,7 @@
+class SomeClass:
+    def __init__(self, value) -> None:
+        self.value=value
+    def some_function(self, number):
+        return self.value*number
+    def some_text_function(self, text):
+        print("some really long text - some really long text - some really long text - some really long text - some really long text - " + text)
