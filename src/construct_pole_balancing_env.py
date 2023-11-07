d7f8e8d22de97afa01dd59cd9041b9c1534639d9
Omar Emara <hn23952@bristol.ac.uk>
2023-11-07 15:37:47 +0000

    fix major bugs with state transition

--- src/construct_pole_balancing_env.py
+++ src/construct_pole_balancing_env.py
@@ -10,48 +10,49 @@ the RL agent to interact with. The following methods are included:
 
 @author: hn23952
 """
-import math 
+import math
 import numpy as np
 
 from gymnasium.envs.classic_control.cartpole import CartPoleEnv
 
+
 class ConstructPoleBalancingEnv(CartPoleEnv):
     """
     Construtor function to create the environment for the RL task for pole-cart
-    problem. 
-    
+    problem.
+
     - Initialise object by calling ConstructPoleBalancingEnv()
     while training:
         - state_transition(agent_action)
         - update_buffer()
         - return_reward()
         - termination_status()
-    
+
     """
+
     # assume that this a reasonable time frame for the agent to act and the env
     # need to update
-    time_delta=0.5
-    
-    def __init__(
-            self,
-            cartmass: float = 1.0, 
-            masspole: float = 0.1, 
-            gravity: float = 9.81, 
-            friction: float = 0.0, 
-            length: float = 0.5, 
-            cart_x_position: float = 0, 
-            cart_velocity: float = 0, 
-            pole_angle: int = 0, 
-            pole_velocity: float = 0,
-            max_iter: int = 100,
-            iteration: int =0,
-            maximum_buffer_size: int = 1000,
-            force_mag: float = 10.0, 
-            render_mode: str = "human"
+    time_delta = 0.5
 
-            )->None:
+    def __init__(
+        self,
+        cartmass: float = 1.0,
+        masspole: float = 0.1,
+        gravity: float = 9.81,
+        friction: float = 0.0,
+        length: float = 0.5,
+        cart_x_position: float = 0,
+        cart_velocity: float = 0,
+        pole_angle: int = 0,
+        pole_velocity: float = 0,
+        max_iter: int = 100,
+        iteration: int = 0,
+        maximum_buffer_size: int = 1000,
+        force_mag: float = 10.0,
+        render_mode: str = "human",
+    ) -> None:
         """
-        use the constructor method to define the parameters for the problem. 
+        use the constructor method to define the parameters for the problem.
         vary the parameters to test your agent for different setups
 
         Parameters
@@ -60,32 +61,32 @@ class ConstructPoleBalancingEnv(CartPoleEnv):
             Default value is 1 kg
 
         masspole : float
-            
+
         gravity : float
             default value is 9.81 N/kg
 
         friction : float
-            default value is frictionless 
+            default value is frictionless
 
         length : float
             default value is 0.3 meters
-        
+
         cart_x_postion : float
-            default to the centre of the grid. The centre of the cart is at 
+            default to the centre of the grid. The centre of the cart is at
             the x coordinate 0
-            
+
         cart_velocity : float
             defaults to stationary
-            
+
         pole_angle : int
             defaults to balanced
-        
+
         pole_velocity : float
             defaults to stationary
-            
+
         max_iter : int
             default to 100 iterations
-            
+
         iterations: int
             starts as 0 and increments with each time state_transition is called
 
@@ -96,46 +97,38 @@ class ConstructPoleBalancingEnv(CartPoleEnv):
 
         """
         super().__init__(render_mode)
-        self.cartmass=cartmass
-        self.masspole=masspole
-        self.friction=friction
-        self.gravity=gravity
-        self.force_mag=force_mag
-        self.length=length
-        self.state=[cart_x_position, cart_velocity, pole_angle, pole_velocity]
-        self.max_iter=max_iter
-        self.iteration=iteration
-        self.maximum_buffer_size=maximum_buffer_size
-        self.prev_angle=None
-        
-    def return_reward(
-            self, 
-            agent_action: float
-            )->float:
-        
-        # return -1*abs(self.state[2])
-        
+        self.cartmass = cartmass
+        self.masspole = masspole
+        self.friction = friction
+        self.gravity = gravity
+        self.force_mag = force_mag
+        self.length = length
+        self.state = [cart_x_position, cart_velocity, pole_angle, pole_velocity]
+        self.max_iter = max_iter
+        self.iteration = iteration
+        self.maximum_buffer_size = maximum_buffer_size
+        self.prev_angle = None
+
+    def return_reward(self, agent_action: float) -> float:
+        return -1 * abs(self.state[2])
+
         # if the last observed angle of the pole is greater is than second to
-        # to last observed pole angle i.e. the RL agent caused the pole to 
+        # to last observed pole angle i.e. the RL agent caused the pole to
         # be farther away from being balanced. Regardless of the direction
-        if abs(self.state[2])>abs(self.prev_angle):
-            # returns a reward negative reward proportional to how farther away
-            # the pole was diverted from being balanced
-            return -1*abs(self.state[2]-self.prev_angle)
-        # if the action made the pole closer to being balanced
-        elif abs(self.state[2])<abs(self.prev_angle):
-            # returns a positive reward proportional to how closer it got the
-            # pole being balanced
-            return abs(self.state[2]-self.prev_angle)
-        # if the action didn't change the angle
-        else:
-            return 0.0
-        
-        
-    def state_transition(
-            self,
-            agent_action: float
-            ):
+        # if abs(self.state[2])>abs(self.prev_angle):
+        #     # returns a reward negative reward proportional to how farther away
+        #     # the pole was diverted from being balanced
+        #     return 0
+        # # if the action made the pole closer to being balanced
+        # elif abs(self.state[2])<abs(self.prev_angle):
+        #     # returns a positive reward proportional to how closer it got the
+        #     # pole being balanced
+        #     return 1
+        # # if the action didn't change the angle
+        # else:
+        #     return 0.0
+
+    def state_transition(self, agent_action: float):
         """
         positive agent action is a force to the right.
 
@@ -151,49 +144,59 @@ class ConstructPoleBalancingEnv(CartPoleEnv):
 
         """
         # update the cart's state as per the agent's action (Force on cart)
-        
+
         # determine if the force should be + or - according to direction
         # received from the agent
         if agent_action == 1:
-            force=self.force_mag
+            force = self.force_mag
         else:
-    
-            force=-self.force_mag
-            
+            force = -self.force_mag
+
         # retrieve the current values of the components of the state
-        cart_position=self.state[0]
-        cart_velocity=self.state[1]
-        pole_angle=self.state[2]
-        self.prev_angle=pole_angle
-        pole_velocity=self.state[3]
-        
+        cart_position = self.state[0]
+        cart_velocity = self.state[1]
+        pole_angle = self.state[2]
+        self.prev_angle = pole_angle
+        pole_velocity = self.state[3]
+
         #  update force according to friction
-        force=force-self.friction
-        
-        total_mass=self.masspole+self.cartmass
-        pole_mass_length=self.masspole*self.length
+        force = force - self.friction
+
+        total_mass = self.masspole + self.cartmass
+        pole_mass_length = self.masspole * self.length
 
         # intermediate variable to faciliate computation of the acc
-        temp = (force + pole_mass_length * pole_velocity ** 2 * np.sin(pole_angle) - pole_velocity * np.cos(pole_angle)) / total_mass
-        
+        temp = (
+            force
+            + pole_mass_length * pole_velocity**2 * np.sin(pole_angle)
+            - pole_velocity * np.cos(pole_angle)
+        ) / total_mass
+
         # update the pole acc and cart acc
-        pole_acceleration = (self.gravity * np.sin(pole_angle) - np.cos(pole_angle) * temp) / (self.length * (4.0 / 3.0 - self.masspole * np.cos(pole_angle) ** 2 / total_mass))
-        cart_acceleration = temp - pole_mass_length * pole_acceleration * np.cos(pole_angle) / total_mass
-    
+        pole_acceleration = (
+            self.gravity * np.sin(pole_angle) - np.cos(pole_angle) * temp
+        ) / (
+            self.length
+            * (4.0 / 3.0 - self.masspole * np.cos(pole_angle) ** 2 / total_mass)
+        )
+        cart_acceleration = (
+            temp
+            - pole_mass_length * pole_acceleration * np.cos(pole_angle) / total_mass
+        )
+
         # Update state variables
         cart_position = cart_position + cart_velocity
         cart_velocity = cart_velocity + cart_acceleration
         pole_angle = pole_angle + pole_velocity
         pole_velocity = pole_velocity + pole_acceleration
 
-        
-        # increment the current iteration 
-        self.iteration+=1
-        self.state=[cart_position, cart_velocity, pole_angle, pole_velocity]
-        
+        # increment the current iteration
+        self.iteration += 1
+        self.state = [cart_position, cart_velocity, pole_angle, pole_velocity]
+
         return self.state
-    
-    def termination_status(self)->bool:
+
+    def termination_status(self) -> bool:
         """
         terminate if the pole is at 90 degree angle with the y-axis from either
         side or if the max iteration is reached
@@ -205,30 +208,26 @@ class ConstructPoleBalancingEnv(CartPoleEnv):
 
         """
         if (
-                self.state[2]>=math.radians(90) or
-                self.state[2]<=math.radians(-90) or
-                self.iteration>=self.max_iter
-                ):
+            self.state[2] >= math.radians(90)
+            or self.state[2] <= math.radians(-90)
+            or self.iteration >= self.max_iter
+        ):
             return 1
         else:
-            
             return 0
-        
+
     def step(self, agent_action):
         """
-            Call to construct the environment and observe states without
-            having to call each method on its own in your script
+        Call to construct the environment and observe states without
+        having to call each method on its own in your script
         """
         # first get the new env observation
-        observation=self.state_transition(agent_action)
+        observation = self.state_transition(agent_action)
         # compute the reward
-        reward=self.return_reward(agent_action)
-        termination=self.termination_status()
+        reward = self.return_reward(agent_action)
+        termination = self.termination_status()
         return observation, reward, termination, False, dict()
-        
+
     def reset(self, *args, **kwargs):
-        self.iteration=0
+        self.iteration = 0
         return super().reset(*args, **kwargs)
-        
-        
-        
