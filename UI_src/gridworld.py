15525dabf00fce56dac32b7c917d4e2b4e63a03f
vishaljoshi24 <we19383@bristol.ac.uk>
2023-11-08 14:51:24 +0000

    Update gridworld.py

--- UI_src/gridworld.py
+++ UI_src/gridworld.py
@@ -41,27 +41,6 @@ with st.expander("Select distributions"):
         sb.lineplot(x=x, y=test_distribution, label='Test')
         st.pyplot(plt)
 
-    #
-    #     bins = np.linspace(-10,10,100)
-    #
-    #     fig_1, (ax_1, ax_2) = plt.subplots(1, 2)
-    #     fig_1.set_figwidth(4)
-    #     fig_1.set_figheight(2)
-    #     ax_1.hist(arr_1, bins, alpha=0.5)
-    #     ax_2.hist(arr_2, bins, alpha=0.5)
-    # col1.pyplot(fig_1)
-
-
-
-
-    #     st.subheader("A uniform distribution")
-    #     arr_2 = np.random.uniform(5, 15, size=20)
-    #     fig_2, ax_2 = plt.subplots()
-    #     fig_2.set_figwidth(2)
-    #     fig_2.set_figheight(1)
-    #     ax_2.hist(arr_2, bins=20)
-    # col2.pyplot(fig_2)
-
 
 import time
 
@@ -82,14 +61,11 @@ for i in range(10):
 col3, col4 = st.columns([2,2])
 
 with st.container():
-    st.button('Up')
-with st.container():
     with col3:
-        st.button('Left')
-    with col4:
+        st.button('Up')
         st.button('Right')
-with st.container():
-    st.button('Down')
+        st.button('Left')
+        st.button('Down')
 
 
 
