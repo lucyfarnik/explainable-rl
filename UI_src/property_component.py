275b84858950c09ce4363a0f09668eda538017fb
vishaljoshi24 <we19383@bristol.ac.uk>
2023-11-11 18:22:53 +0000

    Update property_component.py

--- UI_src/property_component.py
+++ UI_src/property_component.py
@@ -1,33 +1,36 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-import random
+import scipy as sp
+import seaborn as sb
 
 
-class Properties:
+def Properties():
     col1, col2 = st.columns([2, 2])
-
     with st.expander("Select distributions"):
+        with col2:
+            train_mean = st.slider(
+                label='Select a mean for training set',
+                min_value=-10.00,value=0.00, max_value=10.00, key='train_mean', step=0.01)
+
+            train_std_dev = st.slider(
+                label='Select a std dev for training set',
+                min_value=0.01,value=1.00,max_value= 10.00, key='train_std_dev', step=0.01)
+
+            test_mean = st.slider(
+                label='Select a mean for test set',
+                min_value=-10.00, max_value=10.00, key='y slider')
+
+            test_std_dev = st.slider(
+                label='Select a std dev for test set',
+                min_value=0.01,value=1.00, max_value=10.00, key='test_std_dev',step=0.01)
+
         with col1:
             st.subheader("A gaussian distribution")
-            arr_1 = [random.gauss(3, 1) for _ in range(400)]
-            arr_2 = [random.gauss(4, 2) for _ in range(400)]
+            x = np.linspace(-10, 10, 100)
+            train_distribution = sp.stats.norm.pdf(x, loc=train_mean, scale=train_std_dev)
+            test_distribution = sp.stats.norm.pdf(x, loc=test_mean, scale=test_std_dev)
 
-            bins = np.linspace(-10, 10, 100)
-
-            fig_1, (ax_1, ax_2) = plt.subplots(1, 2)
-            fig_1.set_figwidth(4)
-            fig_1.set_figheight(2)
-            ax_1.hist(arr_1, bins, alpha=0.5)
-            ax_2.hist(arr_2, bins, alpha=0.5)
-        col1.pyplot(fig_1)
-
-        with col2:
-            st.write("Test")
-        #     st.subheader("A uniform distribution")
-        #     arr_2 = np.random.uniform(5, 15, size=20)
-        #     fig_2, ax_2 = plt.subplots()
-        #     fig_2.set_figwidth(2)
-        #     fig_2.set_figheight(1)
-        #     ax_2.hist(arr_2, bins=20)
-        # col2.pyplot(fig_2)
+            fig1 = sb.lineplot(x=x, y=train_distribution, label='Train')
+            sb.lineplot(x=x, y=test_distribution, label='Test')
+            st.pyplot(plt)
