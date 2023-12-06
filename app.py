40b4c203fd983aca9a0466117f2766d829bb6228
Jonathan Raines <jr0278@bristol.ac.uk>
2023-12-06 08:07:09 +0000

    Add readme text to homepage

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from st_pages import Page, show_pages, add_page_title
+import streamlit as st
+from st_pages import Page, show_pages
 
-add_page_title("Explainable Reinforcement Learning")  # By default this adds indentation
 
-# Now to specify which pages should be in the sidebar and what their titles/icons are
+def app():
+    """The entry point of the streamlit app."""
 
-show_pages(
-    [
-        Page("src/page_pole.py", "Pole"),
-        Page("src/page_grid_world.py", "Grid World"),
-    ]
-)
+    # Read the readme file and render it as markdown as a landing page.
+    with open("README.md", "r") as f:
+        readme = f.read()
+
+    st.markdown(readme)
+
+    # Now to specify which pages should be in the sidebar and what their titles/icons are
+    show_pages(
+        [
+            Page("src/page_pole.py", "Pole"),
+            Page("src/page_grid_world.py", "Grid World"),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    app()
